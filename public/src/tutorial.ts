69f45c49ac78ae6f057b5e3d16d10065aa0a38dd
Artworqq <keysub.kevin@gmail.com>
2025-11-11 21:47:08 -0500

    Delete public/src/tutorial.ts

--- public/src/tutorial.ts
+++
@@ -1,6 +0,0 @@
-const continueBtn = document.getElementById('continue-btn');
-if (continueBtn) {
-  continueBtn.addEventListener('click', () => {
-    window.location.href = '/character-selector.html';
-  });
-}
